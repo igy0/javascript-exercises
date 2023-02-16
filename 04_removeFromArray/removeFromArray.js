ee3ce4b1f068ac834fc4b01979c464d997d5b821
Igy Lizardo <igylizardo@gmail.com>
2023-02-15 18:21:29 -0800

    solutions for exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove) {
+    let a;
+    for (let j = 1; j <= arguments.length;j++) {
+        a = arguments[j];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === a) {
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+    else if (typeof(a) != "number" || typeof(b) != "number") {
+        return "ERROR"
+    }
+    else {
+        let i = Math.min(a,b);
+        for (; i <=Math.max(a,b); i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
